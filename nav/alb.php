5aa29b773a6da66c0d87091500e3176ecc88ab39
Илья <id.ilichev@mail.ru>
2025-07-08 17:34:20 +0300

    Create alb.php

---
+++ nav/alb.php
@@ -0,0 +1,37 @@
+<?php require "header.php";?>
+        <main>
+            <section>
+                <h2>Галерея</h2>
+
+                <article>
+                    <div id="date">08.01.2005</div>
+                    <a href="#">Статья 1 - article</a>
+                    <p>Текст статьи бла бла бла</p>
+                </article>
+
+                <article>
+                    <div id="date">09.01.2009</div>
+                    <a href="#">Статья 2 - article</a>
+                    <p>Текст статьи бла бла бла</p>
+                </article>
+
+                <article>
+                    <div id="date">22.03.2021</div>
+                    <a href="#">Статья 3 - article</a>
+                    <p>Текст статьи бла бла бла</p>
+                </article>
+
+                <article>
+                    <div id="date">29.11.2025</div>
+                    <a href="#">Статья 4 - article</a>
+                    <p>Текст статьи бла бла бла</p>
+                </article>
+
+                <article>
+                    <div id="date">06.06.2006</div>
+                    <a href="#">Статья 5 - article</a>
+                    <p>Текст статьи бла бла бла</p>
+                </article>
+            </section>
+        </main>
+<?php require "footer.php";?>
